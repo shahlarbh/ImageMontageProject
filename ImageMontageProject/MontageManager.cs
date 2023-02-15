00372a804a9b0bad2c225078762093ca08b075ac
shahlarbh <shahlarbh@code.edu.az>
2023-02-15 13:56:52 +0400

    Add project files.

---
+++ ImageMontageProject/MontageManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageMontageProject
+{
+    internal class MontageManager
+    {
+        internal Bitmap ColorFormatting(Bitmap image, int red, int green, int blue)
+        {
+            int r, g, b;
+
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    Color pixelColor = image.GetPixel(i, j);
+
+                    r = pixelColor.R + red;
+                    r = r % 255;
+                    g=pixelColor.G + green;
+                    g = g % 255;
+                    b= pixelColor.B + blue;
+                    b = b % 255;
+
+                    Color newColor = Color.FromArgb(r, g, b);
+
+                    image.SetPixel(i, j, newColor);
+                }
+            }
+
+            return image;
+        }
+
+        internal void SaveFile(Bitmap image)
+        {
+            Console.Write("Enter the file name:");
+            string newImage = Console.ReadLine() + ".jpeg";
+
+            image.Save(newImage);
+
+            Console.WriteLine("Image saved");
+        }
+    }
+}
---
+++ ImageMontageProject/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Drawing;
+
+namespace ImageMontageProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MontageManager montageManager = new MontageManager();
+
+            string command = "";
+
+            while (command != "end")
+            {
+                Console.Write("Enter location:");
+                string imageLocation = Console.ReadLine();
+
+                Bitmap image = new Bitmap(imageLocation);
+
+                Console.WriteLine($"1.Random Filter\n2.Custom Filter");
+
+                start:
+
+                command = Console.ReadLine();
+
+                if (command.Equals("1"))
+                {
+                    var random = new Random();
+                    int r = random.Next(0, 255);
+                    int g = random.Next(0, 255);
+                    int b = random.Next(0, 255);
+
+                    Bitmap newRandomImage = montageManager.ColorFormatting(image, r, g, b);
+                    montageManager.SaveFile(newRandomImage);
+                    image.Dispose();
+                }
+
+                else if (command.Equals("2"))
+                {
+                    Console.Write("Red:");
+                    int red = int.Parse(Console.ReadLine());
+                    Console.Write("Green:");
+                    int green = int.Parse(Console.ReadLine());
+                    Console.Write("Blue:");
+                    int blue = int.Parse(Console.ReadLine());
+
+                    Bitmap newImage = montageManager.ColorFormatting(image, red, green, blue);
+                    montageManager.SaveFile(newImage);
+                    image.Dispose();
+                }
+
+                else
+                {
+                    Console.Write("Choose the correct command:");
+                    goto start;
+                }
+            }
+        }
+    }
+}
